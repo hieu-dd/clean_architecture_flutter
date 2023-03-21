5d1b25ff82c95b8521e32c29e92ae1def50d6d71
14H <doduchieu.kstn@gmail.com>
2023-03-21 14:39:01 +0700

    Update ui

--- lib/features/number_trivia/presentation/pages/number_trivia_page.dart
+++ lib/features/number_trivia/presentation/pages/number_trivia_page.dart
@@ -14,76 +14,78 @@ class NumberTriviaPage extends ConsumerWidget {
       appBar: AppBar(
         title: Text('Number Trivia'),
       ),
-      body: Column(
-        children: [
-          Container(
-            width: double.infinity,
-            height: 400,
-            child: state is Empty
-                ? Container()
-                : state is Loading
-                    ? const Center(
-                        child: CircularProgressIndicator(),
-                      )
-                    : state is Error
-                        ? Center(
-                            child: Text(state.message),
-                          )
-                        : loadedWidget(state as Loaded),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: TextField(
-              controller: numberController,
-              decoration: new InputDecoration(
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                      color: Theme.of(context).focusColor, width: 1.0),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 400,
+              child: state is Empty
+                  ? Container()
+                  : state is Loading
+                      ? const Center(
+                          child: CircularProgressIndicator(),
+                        )
+                      : state is Error
+                          ? Center(
+                              child: Text(state.message),
+                            )
+                          : loadedWidget(state as Loaded),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextField(
+                controller: numberController,
+                decoration: new InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Theme.of(context).focusColor, width: 1.0),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Theme.of(context).dividerColor, width: 1.0),
+                  ),
+                  hintText: 'Mobile Number',
                 ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                      color: Theme.of(context).dividerColor, width: 1.0),
-                ),
-                hintText: 'Mobile Number',
               ),
             ),
-          ),
-          Row(
-            children: [
-              SizedBox(
-                width: 10,
-              ),
-              Expanded(
-                child: ElevatedButton(
-                    onPressed: () {
-                      final number = int.tryParse(numberController.text);
-                      if (number == null) return;
-                      ref
-                          .read(numberTriviaProvider.notifier)
-                          .getConcreteNumberTrivia(numberController.text);
-                    },
-                    child: Text("Search")),
-                flex: 1,
-              ),
-              SizedBox(
-                width: 10,
-              ),
-              Expanded(
-                child: ElevatedButton(
-                    onPressed: () {
-                      ref
-                          .read(numberTriviaProvider.notifier)
-                          .getRandomNumberTrivia();
-                    },
-                    child: Text("Random")),
-                flex: 1,
-              ),
-              SizedBox(
-                width: 10,
-              ),
-            ],
-          ),
-        ],
+            Row(
+              children: [
+                SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: ElevatedButton(
+                      onPressed: () {
+                        final number = int.tryParse(numberController.text);
+                        if (number == null) return;
+                        ref
+                            .read(numberTriviaProvider.notifier)
+                            .getConcreteNumberTrivia(numberController.text);
+                      },
+                      child: Text("Search")),
+                  flex: 1,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: ElevatedButton(
+                      onPressed: () {
+                        ref
+                            .read(numberTriviaProvider.notifier)
+                            .getRandomNumberTrivia();
+                      },
+                      child: Text("Random")),
+                  flex: 1,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
